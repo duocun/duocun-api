54601dfe21fbdac9a7e6ef3aebdc0c87e7149f1c
zlkca <likzhang@gmail.com>
2020-08-31 14:49:43 -0400

    fix develop env bug

--- server/server.ts
+++ server/server.ts
@@ -97,7 +97,7 @@ const apimw = new ApiMiddleWare();
 const utils = new Utils();
 const cfg = new Config();
 
-const SVC_PATH = process.env.ENV === 'localhost'? process.env.SVC_PATH : '';
+const SVC_PATH = process.env.ENV === 'local'? process.env.SVC_PATH : '';
 
 const app = express();
 
